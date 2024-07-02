1cd923a45ec48f9407b67e197f2f5d776e08c81d
mustafaduyarer <mustafaduyarer@gmail.com>
2024-07-02 10:11:43 +0200

    hide toggle

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import JournalList from "./journals/journal_list";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
+        <h1>DevCamp React Starterby Mustafa</h1>
         <h2>React Skeleton</h2>
+        <JournalList heading= "list 1"/>
+       
       </div>
     );
   }
 }
+
